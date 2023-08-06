c1340af306d23b156fc2bde82a12420073bfee7f
M1seryy <62029606+M1seryy@users.noreply.github.com>
2023-08-06 18:08:55 +0300

    finish

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,12 +1,14 @@
 import { findByName } from 'API/trends';
-import React, {  useRef, useState } from 'react';
-import { Link, useLocation } from 'react-router-dom';
+import React, { useEffect, useRef, useState } from 'react';
+import { Link, useLocation, useSearchParams } from 'react-router-dom';
 import styles from './movies.module.css';
 
 const Movies = () => {
   const [movies, setMovies] = useState([]);
+  const [params, setSearchParams] = useSearchParams('');
+  const query = params.get('query');
   const movieQuery = useRef();
-const location = useLocation()
+  const location = useLocation();
   const onHandle = e => {
     e.preventDefault();
     findByName(movieQuery.current.value)
@@ -14,17 +16,36 @@ const location = useLocation()
       .catch(err => console.log(err));
   };
   console.log(movies);
+
+  const onValueHandler = e => {
+    setSearchParams({ query: e.target.value });
+  };
+  useEffect(() => {
+    if (query !== null) {
+      findByName(query)
+        .then(res => setMovies(res.results))
+        .catch(err => console.log(err));
+    } else return;
+  }, []);
   return (
     <div>
       <form onSubmit={onHandle}>
-        <input ref={movieQuery} type="text" className={styles.input} />
+        <input
+          value={query}
+          ref={movieQuery}
+          onChange={onValueHandler}
+          type="text"
+          className={styles.input}
+        />
         <input type="submit" value="Find" />
       </form>
       <ul>
         {movies.map(item => {
           return (
             <li key={item.id}>
-              <Link to={`/movies/${item.id}`} state={location}>{item.title}</Link>
+              <Link to={`/movies/${item.id}`} state={location}>
+                {item.title}
+              </Link>
             </li>
           );
         })}
